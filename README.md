4ee9b52a3a7e8b65688b4b4428a2ba92c2983fdb
TakatokiOshima <ki13sd6p@gmail.com>
2022-04-24 01:46:42 +0900

    add: ginの追加と動作確認用に関数作成

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ docker-compose up -d --build
 docker-compose exec golang /bin/bash
 go run main.go
 ```
+
+```
+curl http://localhost:8080
+```
--- src/main.go
+++ src/main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
+func test(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, gin.H{"message": "Hello World!"})
+}
+
 func main() {
+	router := gin.Default()
+	router.GET("/", test)
+	router.Run("localhost:8080")
 }
